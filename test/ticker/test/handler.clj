6bc6a3cab9e28cc2e85d0f317772592230dabfd4
Kiaresch Mussawisade <kiaresch.mussawisade@co-in.de>
2014-06-13 10:31:34 +0200

    mongodb mocked

--- test/ticker/test/handler.clj
+++ test/ticker/test/handler.clj
@@ -1,12 +1,14 @@
 (ns ticker.test.handler
   (:use clojure.test
         ring.mock.request
-        ticker.handler))
+        ticker.handler)
+  (:require [ticker.models.message :as message]))
 
 (deftest test-app
+  (with-redefs [message/messages (fn [] "")]
   (testing "main route"
     (let [response (app (request :get "/"))]
-      (is (= (:status response) 200))))
+      (is (= (:status response) 200)))))
 
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
