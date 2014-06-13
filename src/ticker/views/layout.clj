c960926986c295f79f772dd82aa2bd5f0729e49e
Theo Pack <tf.pack@gmail.com>
2014-06-13 11:07:37 +0200

    add form

--- src/ticker/views/layout.clj
+++ src/ticker/views/layout.clj
@@ -1,5 +1,6 @@
 (ns ticker.views.layout
-  (:require [hiccup.page :refer [html5 include-css]]))
+  (:require [hiccup.page :refer [html5 include-css]]
+            [hiccup.form :refer :all]))
 
 (defn common [& body]
   (html5
@@ -13,4 +14,13 @@
    (for [{:keys [username message]} messages]
      [:div
       [:h2 message]
-      [:p username]])])
+      [:p username]])]
+  (form-to [:post "/"]
+     [:p "Name:"]
+     (text-field "username" "Benutzername goes here")
+
+     [:p "Message:"]
+     (text-area {:rows 10 :cols 40} "message" "Message goes here")
+
+     [:br]
+     (submit-button "comment")))
